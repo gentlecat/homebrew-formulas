ad89eedad8317c05afb0ff4ec986707f196ec71b
Roman Tsukanov <roman@tsukanov.me>
2018-04-28 16:59:26 -0700

    Make the clippy formula HEAD-only

--- Formula/clippy.rb
+++ Formula/clippy.rb
@@ -1,8 +1,6 @@
 class Clippy < Formula
   desc "Make Clippy say things"
   homepage "https://code.roman.zone/clippy"
-  url "https://github.com/gentlecat/clippy/archive/v1.0.tar.gz"
-  sha256 "f3ae8d3d4564a41b6e2a21f237d2f2b104f48108591e8b83497500182a3ab3a4"
   head "https://github.com/gentlecat/clippy.git"
 
   depends_on "go" => :build
